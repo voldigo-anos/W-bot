6409d86c7e9bcc35c40216620c2f9b0d3e4afbb5
Christus-Ten <voldigoa82@gmail.com>
2026-03-11 10:16:09 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const { spawn } = require("child_process");
 const path = require("path");
+const http = require("http");
+
+const p = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('NixBot is running ✅');
+}).listen(p);
 
 function startBot() {
   const child = spawn(process.execPath, [path.join(__dirname, "nix.js")], {
@@ -10,10 +18,8 @@ function startBot() {
 
   child.on("exit", (code) => {
     if (code === 2) {
-      console.log("🔄 Bot requested restart restarting...");
-      setTimeout(startBot, 1500);
+      setTimeout(startBot, 1000);
     } else {
-      console.log(`🔄 Bot exited with code ${code}`);
       process.exit(code || 0);
     }
   });
